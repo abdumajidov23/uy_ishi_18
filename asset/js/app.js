a3b42a5459f7d8a195ac705ddd8d11693cdcd9ad
abdumajidov23 <alijonabdumajidov09@gmail.com>
2024-07-13 19:44:12 +0500

    uy_ishi_18

---
+++ asset/js/app.js
@@ -0,0 +1,3 @@
+// console.log("\x1bmServer is working on port: \x1b[32m", 4000)
+// console.log("\x1bmServer is working on port: \x1b[32m", 1000)
+console.log("\x1b[32mServer is working on port: \x1b[32m", 4000)
